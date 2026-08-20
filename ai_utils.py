bee035bbe1b53f213a5a3baa3374b6c4153157fe
Priyanka PAL <priyankapal29702@gmail.com>
2026-08-20 14:49:41 +0530

    updated

--- ai_utils.py
+++ ai_utils.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from langchain_mistralai import ChatMistralAI
 import streamlit as st
 from langchain_core.messages import HumanMessage
@@ -51,53 +50,3 @@ Keep it concise and professional.
     response = client.invoke(
         [HumanMessage(content=prompt)]
     )
-
-=======
-from langchain_mistralai import ChatMistralAI
-from langchain_core.messages import HumanMessage
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-client = ChatMistralAI(
-    api_key=os.getenv("MISTRAL_API_KEY"),
-    model="mistral-small"
-)
-
-def generate_ai_recommendation(df):
-
-    if df.empty:
-        return "No data available for generating insights."
-
-    total_sales = df["Sales"].sum()
-    avg_sales = df["Sales"].mean()
-    total_customers = df["Customers"].sum()
-
-    promo_effect = df.groupby("Promo")["Sales"].mean().to_dict()
-    holiday_effect = df.groupby("Holiday")["Sales"].mean().to_dict()
-
-    prompt = f"""
-You are a business analyst.
-
-Analyze this retail dataset summary:
-
-Total Sales: {total_sales}
-Average Sales: {avg_sales}
-Total Customers: {total_customers}
-Promotion Impact: {promo_effect}
-Holiday Impact: {holiday_effect}
-
-Provide:
-1. Key insights
-2. Business recommendations
-
-Keep it concise and professional.
-"""
-
-    response = client.invoke(
-        [HumanMessage(content=prompt)]
-    )
-
->>>>>>> 219835bee29bc6a6c72333761d73d9695c1c441f
-    return response.content
